347890fc528e12bf7fe9dc583237a1205c328c85
leontoys <svliyon@gmail.com>
2025-03-24 10:39:09 +1100

    3.2 backend products and product routes

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dontenv from 'dotenv'
+dontenv.config()
+import products  from './data/products.js'
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/',(req,res)=>{
+    res.send('API running')
+})
+
+app.get('/api/products',(req,res)=>{
+    res.json(products)
+})
+
+app.get('/api/products/:id',(req,res)=>{
+    const {id} = req.params
+    const proudct = products.find(proudct => proudct._id = id)
+    res.json(proudct)
+})
+
+app.listen(port,()=>{
+    console.log(`app listening http://localhost:${port}/`)
+})
