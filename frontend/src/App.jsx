af7577f0291691f810cd8decd5b4dceba93f7b83
leontoys <svliyon@gmail.com>
2025-03-24 09:26:56 +1100

    react router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,21 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className='py-3'>
+    <>      <main className='py-3'>
         <Container>
-          <HomeScreen></HomeScreen>
+          <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Router>
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,23 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
+          <Navbar.Brand as={Link} to='/'>
             <img src={logo}/>
             ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
+              <Nav.Link to='/cart' as={Link}>
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
+              <Nav.Link to='/login' as={Link}>
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
