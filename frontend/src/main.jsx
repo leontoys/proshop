cab8898014490afb00dfef0db79f2c0224b8b4ab
leontoys <svliyon@gmail.com>
2025-03-24 06:53:58 +1100

    added-custom bootstrap theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
