9498d72c2c67d97c62a566b96473e12eea53697c
leontoys <svliyon@gmail.com>
2025-03-25 07:59:06 +1100

    4.10 product routes and controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,29 @@
+import asyncHandler from 'express-async-handler'
+//load model
+import Product from '../models/productModel.js'
+
+//get all
+const getProducts = asyncHandler(async (req,res)=>{
+    const products = await Product.find({})//get from backend
+    if(products){
+        return res.json(products)
+    }
+    //error
+    res.status(404)
+    throw new Error('Resource not found');
+})
+
+//get one product
+const getProduct = asyncHandler(async(req,res)=>{
+    const {id} = req.params
+    //const proudct = products.find(proudct => proudct._id = id) - this was to get from the file
+    const product = await Product.findById(id)
+    if(product){
+        return res.json(product)
+    }
+    //error
+    res.status(404)
+    throw new Error('Resource not found');
+})
+
+export {getProduct,getProducts}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,11 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import {getProduct,getProducts} from '../controllers/productController.js'
 
 const router = express.Router()
 //all api paths to be copied over from index.js to here
 
-router.get('/',asyncHandler(async (req,res)=>{
-    const products = await Product.find({})//get from backend
-    if(products){
-        return res.json(products)
-    }
-    //error
-    res.status(404)
-    throw new Error('Resource not found');
-}))
+router.get('/',getProducts)//.get(getProducts)
 
-router.get('/:id',asyncHandler(async(req,res)=>{
-    const {id} = req.params
-    //const proudct = products.find(proudct => proudct._id = id) - this was to get from the file
-    const product = await Product.findById(id)
-    if(product){
-        return res.json(product)
-    }
-    //error
-    res.status(404)
-    throw new Error('Resource not found');
-}))
+router.get('/:id',getProduct)
 
 export default router
